7da541159a83c084154829469716893df9a175ff
Sujani Wijesundera <sujani75@gmail.com>
2020-06-08 13:10:33 -0600

    Sithmi commeted

--- src/main/java/Cucumber/automation/App.java
+++ src/main/java/Cucumber/automation/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello the git way" );
+        System.out.println( "Problem in project name" );
     }
 }
