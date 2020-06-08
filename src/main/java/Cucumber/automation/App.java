8e79a6766335fcb55c26b14198b1441c69328b9d
Sujani Wijesundera <sujani75@gmail.com>
2020-06-08 13:50:30 -0600

    Architech Asian development

--- src/main/java/Cucumber/automation/App.java
+++ src/main/java/Cucumber/automation/App.java
@@ -10,5 +10,12 @@ public class App
     {
         System.out.println( "Hello the git way" );
         System.out.println( "Problem in project name" );
+        getMessage();
+        
+    }
+    
+    private static void getMessage() {
+    	
+    	System.out.println("This is get Message");
     }
 }
